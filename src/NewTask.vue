786c8ec572e87592faa6519072688019fe04fb1c
Michal <miketopolsky1@interia.pl>
2018-03-24 14:41:22 +0100

    Changes to New Task appeareance

--- src/NewTask.vue
+++ src/NewTask.vue
@@ -1,13 +1,18 @@
 <template>
-    <md-list-item>
-        <md-button @click="addTask" class="md-fab md-primary">
-            <md-icon>add</md-icon>
-        </md-button>
-        <md-field>
-            <label>New task</label>
-            <md-input v-model="newTask" md-counter="30"></md-input>
-        </md-field>
-    </md-list-item>
+    <md-card>
+        <md-card-header><div class="md-title">New task</div></md-card-header>
+        <md-card-content>
+            <md-field>
+                <label>New task</label>
+                <md-input v-model="newTask" md-counter="30"></md-input>
+            </md-field>
+        </md-card-content>
+        <md-card-actions>
+            <md-button class="md-raised md-accent" @click="addTask">
+                Add
+            </md-button>
+        </md-card-actions>
+    </md-card>
 </template>
 
 <script>
